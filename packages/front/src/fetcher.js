4ed5dd6cbb8277c81f4168da756585d412207f98
billyct <billy_allen@126.com>
2021-01-25 21:08:53 +0800

    chg: update fetcher with unfetch

--- packages/front/src/fetcher.js
+++ packages/front/src/fetcher.js
@@ -1,21 +1,36 @@
+import fetch from 'unfetch'
+
+/**
+ * @param {string} query
+ * @returns {Promise<never>}
+ */
 const fetcher = query => {
   const options = {
     method: 'POST',
     headers: {
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
     },
-    body: JSON.stringify({ query })
+    body: JSON.stringify({query}),
   }
 
-  return fetch('/graphql',options)
-    .then(async r => {
-      const res = await r.json()
-
-      if ('errors' in res) {
-        throw new Error(res.errors[0].message)
+  return fetch('/graphql', options)
+    .then(response => {
+      if (response.ok) {
+        return response.json()
       }
 
-      return res.data
+      // non-2xx 的 HTTP 请求
+      const err = new Error(response.statusText)
+      err.response = response
+      return Promise.reject(err)
+    })
+    .then(data => {
+      // 2xx 的 HTTP 请求，但是有错误
+      if ('errors' in data) {
+        return Promise.reject(new Error(data.errors[0].message))
+      }
+
+      return data.data
     })
 }
 
