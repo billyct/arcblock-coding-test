3a35a31981d35bd3f6fb0eeae7c24a76dc8aab13
billyct <billy_allen@126.com>
2021-01-25 20:48:33 +0800

    tests: mock wouter module

--- packages/front/src/__mocks__/wouter.js
+++ packages/front/src/__mocks__/wouter.js
@@ -1,9 +1,12 @@
 const wouter = jest.createMockFromModule('wouter')
 
-let __mockSetLocationFn = 'jest.fn()'
+let __setLocationFn = jest.fn()
+let __useRouteFn = jest.fn()
 
-wouter.__mockSetLocation = fn => __mockSetLocationFn = fn
+wouter.__mockUseRoute = fn => __useRouteFn = fn
+wouter.__mockSetLocation = fn => __setLocationFn = fn
 wouter.Link = props => <a {...props}/>
-wouter.useLocation = () => ([jest.fn(), __mockSetLocationFn])
+wouter.useLocation = () => ([jest.fn(), __setLocationFn])
+wouter.useRoute = r => __useRouteFn(r)
 
 module.exports = wouter
